0b03010cf657ede7f187bdab08fc50f6d1a3d1d1
duran.hsieh <duran.hsieh@afusion.com>
2017-10-13 12:42:48 +0800

    update

---
+++ StepTwo/AspDotNetCoreExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AspDotNetCoreExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls("http://*:5000;http://www.xxx.com;https://10.xxx.3.114")
+                .Build();
+    }
+}
