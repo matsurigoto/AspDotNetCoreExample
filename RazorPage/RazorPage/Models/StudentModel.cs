3ecb1f01be73b36925e262d2ef8ad3ed3024f9a5
duran.hsieh <duran.hsieh@afusion.com>
2017-10-13 15:29:10 +0800

    add razor page example

---
+++ RazorPage/RazorPage/Models/StudentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RazorPage.Models
+{
+    public class StudentModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ RazorPage/RazorPage/Pages/Create.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPage.Models;
+
+namespace RazorPage.Pages
+{
+    public class CreateModel : PageModel
+    {
+
+        [BindProperty]
+        public StudentModel Student { get; set; }
+
+        public string GetMessage { get; set; }
+        public string PostMessage { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            GetMessage = "GET";
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            PostMessage = Student.Name;
+            return Page();
+        }
+    }
+}
