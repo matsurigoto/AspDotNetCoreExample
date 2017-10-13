54fe3c4fd6899fe7d226931f9427aed417bdebe2
duran.hsieh <duran.hsieh@afusion.com>
2017-10-13 15:04:40 +0800

    windows service

---
+++ WindowsServices/WindowsServices/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+
+namespace WindowsServices
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+            var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(pathToContentRoot)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.RunAsService();
+        }
+    }
+}
